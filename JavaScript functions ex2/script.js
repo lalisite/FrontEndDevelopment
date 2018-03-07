2210e8fbdacd23ad4974461e91101f31bea15ee8
liorgad <liorgad@gmail.com>
2018-03-07 11:21:25 +0200

    js functions ex2

--- JavaScript functions ex2/script.js
+++ JavaScript functions ex2/script.js
@@ -8,13 +8,35 @@ var calcLargerInt = function(num1,num2) {
     return num2;
 }
 
+var sort = function(num1,num2,num3){
+    var initialArray =[num1,num2,num3];
+    var finalResultArray =[];
+    var max = num1;
+
+    var largest = calcLargerInt(num1,num2);
+    largest = calcLargerInt(largest,num3);
+    finalResultArray.unshift(largest);
+    var index = initialArray.indexOf(largest);
+    initialArray.splice(index,1);
+    largest = calcLargerInt(initialArray[0],initialArray[1]);
+    finalResultArray.unshift(largest);
+    var index = initialArray.indexOf(largest);
+    initialArray.splice(index,1);
+    finalResultArray.unshift(initialArray[0]);
+
+    return finalResultArray;
+}
+
 $(function () {    
     $("#calculate-btn").click(
         function (){            
             var firstNumStr = $("#firstNum").val();
             var secondNumStr = $("#secondNum").val();
-            var result = calcLargerInt(parseInt(firstNumStr),parseInt(secondNumStr));
+            var thirdNumStr = $("#thirdNum").val();
+            var result = sort(parseInt(firstNumStr),parseInt(secondNumStr),parseInt(thirdNumStr));
             $("#result").val(result);
+            var html = '<h1>' +result + '</h1>';            
+            $("#resultDiv").html(html);
         }
     );
 });
